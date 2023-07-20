4449247e6a76086c3695169b969ca1c6fe77c30e
muzammil2208 <muzammilsk90@gmail.com>
2023-07-20 12:55:07 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+export default function Navbar()
+{
+    return(
+        <nav className="navbar">
+            <p>Muzammil Khan</p>
+            <div>
+                <a>About</a>
+                <a>Contact</a>
+                <button>View Work</button>
+            </div>
+            
+        </nav>
+    )
+}
