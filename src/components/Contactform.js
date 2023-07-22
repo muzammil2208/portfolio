5e6cb858caaecea8be0835f710af0daa89007c28
muzammil2208 <muzammilsk90@gmail.com>
2023-07-23 01:26:31 +0530

    basic layout completed

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -5,12 +5,19 @@ export default function Contactform()
             <form className="Contact-form">
             <div className="form-title">Want to get in touch?</div>
             <div className="form-inputs">
-                <label>Name</label>
-                <input type="text"/>
-                <label>Email</label>
-                <input type='email'/>
-                <label>message</label>
-                <textarea rows='10' cols="8"/>
+                <div className="form-entry">
+                    <label>Name</label>
+                    <input type="text" placeholder='enter your name'/>
+                </div>
+                <div className="form-entry">
+                    <label>Email</label>
+                    <input type="email" placeholder="enter you email"/>
+                </div>
+                <div className="form-message">
+                    <label>Message</label>
+                    <textarea rows="5" cols="7" placeholder="enter your message here"/>
+                </div>
+                <button className='form-button' type="submit">Submit</button>
             </div>
         </form>
         </div>
